d910d4333cbfe2e6e9d90bc749ae102b4b24306d
jose guilherme <gui.irlaurent19@gmail.com>
2022-04-06 23:10:10 -0300

    Criando e adicionando projetos java  do desafio gftqa1

---
+++ Mediateste.java
@@ -0,0 +1,22 @@
+package com.dio.desafio.codigo;
+
+import java.util.Scanner;
+
+public class Mediateste {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double A, B, media;
+
+
+        A = Double.parseDouble(sc.next());
+        B = Double.parseDouble(sc.next());
+
+        media = (A * 3.5 + B * 7.5)/11;
+
+        System.out.printf("MEDIA = " +String.format("%.5f", media));
+        System.out.println();
+
+    }
+
+}
---
+++ Xadrez.java
@@ -0,0 +1,19 @@
+package com.dio.desafio.codigo;
+
+import  java.util.Scanner;
+
+public class Xadrez {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int line = sc.nextInt();
+        int column = sc.nextInt();
+
+        int color = ((line+column)%2==0) ? 1 : 0;
+
+        System.out.println(color);
+
+        sc.close();
+    }
+}
